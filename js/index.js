b1fac44aef77a5cb5cc893ac31d83afc96935f73
Adryann Layon <adryann.layon7@gmail.com>
2023-01-05 22:41:37 -0300

    commented codes

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import './router.js'
+
+// const routes = {
+//   '/': '/pages/home.html',
+//   '/about': '/pages/about.html',
+//   '/contact': '/pages/contact.html',
+//   404: '/pages/404.html'
+// }
+
+// handle()
+// window.addEventListener('popstate', handle)
+// window.route = () => route()
